0e21b671074d67e92409ddd6948f5884e2065e4d
Natalie <natalie.coleman39@gmail.com>
2021-09-13 07:15:27 -0400

    Publishing gameoflife app

---
+++ gameoflife/precache-manifest.67adc4511352fe61c5f8e48097bd4f60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/gameoflife/favicon.ico"
+  },
+  {
+    "revision": "7c8034866c9a6723c62fc5260900421e",
+    "url": "/gameoflife/index.css"
+  },
+  {
+    "revision": "6efef377e8d85cdbf7f4c5fee37bd379",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/gameoflife/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/gameoflife/manifest.json"
+  },
+  {
+    "revision": "21520623fc57949818cb",
+    "url": "/gameoflife/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "21520623fc57949818cb",
+    "url": "/gameoflife/static/js/main.21520623.chunk.js"
+  },
+  {
+    "revision": "02997676e727076f9365",
+    "url": "/gameoflife/static/js/runtime~main.02997676.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/gameoflife/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
